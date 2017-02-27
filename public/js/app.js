316cfbacd94f8bcddbbf42d90f2cb0a8194901ed
TimFung93 <timothy.fung.04@gmail.com>
2017-02-26 22:40:19 -0500

    commit

---
+++ public/js/app.js
@@ -0,0 +1,96 @@
+
+
+
+$(window).on('load', function() { 
+
+
+function runTyping() {
+		let element = document.querySelector('#output');
+		let typeSpeed = 100; // 80 ms
+		let deleteSpeed = 30; // 30 ms
+		let deleteAfter = 1000; // 1 second
+		let items = [
+		  "I learn quickly",
+		  "I enjoy challenges",
+		  "I have HUSTLE",
+		  "Uber Flip, Please Hire me! :)"
+		];
+		/* END SETTINGS */
+
+		let sentence = 0;
+		let currentChar = 0;
+		let direction = 1;
+		let deleteInterval = null;
+
+		function type() {
+		  if (sentence >= items.length) {
+		    sentence = 0;
+		  }
+
+		  let chars = items[sentence].split('');
+
+		  setTimeout(function() {
+		    if (currentChar >= chars.length) {
+		      setTimeout(function() {
+		        sentence++;
+
+		        deleteInterval = setInterval(function() {
+		        	element.innerHTML = element.innerHTML.substr(0, currentChar - 1);
+		          	
+		          currentChar--;
+
+		          if (currentChar === 0) {
+		            clearInterval(deleteInterval);
+		            type();
+		          }
+		        }, deleteSpeed);
+		      }, deleteAfter);
+
+		      return;
+		    }
+
+		    element.innerHTML += chars[currentChar];
+		    currentChar++;
+
+		    type();
+		  }, typeSpeed);
+		}
+
+		type();
+	}
+runTyping();
+
+	})
+
+// $( document).ready(function() {
+    
+
+
+// // $('.button-collapse').sideNav();
+
+// $('#output').click(function() {
+
+// runTyping();
+// console.log('i work')
+
+// })
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
